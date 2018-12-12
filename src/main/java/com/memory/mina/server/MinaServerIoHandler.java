5c53576b71e06a13de83c0b54c19543a06386932
memory-win <280247489@qq.com>
2018-12-12 17:17:57 +0800

    握手协议

--- src/main/java/com/memory/mina/server/MinaServerIoHandler.java
+++ src/main/java/com/memory/mina/server/MinaServerIoHandler.java
@@ -8,6 +8,7 @@ import org.apache.mina.core.service.IoHandlerAdapter;
 import org.apache.mina.core.session.IdleStatus;
 import org.apache.mina.core.session.IoSession;
 
+import java.security.MessageDigest;
 import java.util.Map;
 
 public class MinaServerIoHandler extends IoHandlerAdapter {
@@ -60,7 +61,7 @@ public class MinaServerIoHandler extends IoHandlerAdapter {
     @Override
     public void messageReceived(IoSession session, Object message) throws Exception {
         super.messageReceived(session, message);
-        System.out.println("messageReceived: "+ message);
+        System.out.println("messageReceived: \n"+ message);
         //处理客户端消息
         doMessage(session, message);
     }
@@ -68,8 +69,9 @@ public class MinaServerIoHandler extends IoHandlerAdapter {
     @Override
     public void messageSent(IoSession session, Object message) throws Exception {
         super.messageSent(session, message);
+        System.out.println("messageSent: "+ message);
     }
-
+    private boolean hasHandshake = false;
     private void doMessage(IoSession session, Object message) throws Exception {
         if(message instanceof IMMessage){
             doIMMessage((IMMessage) message);
@@ -78,8 +80,31 @@ public class MinaServerIoHandler extends IoHandlerAdapter {
         }else if(message instanceof CloseMessage){
             doCloseMessage(session, (CloseMessage) message);
         }else{
-            System.out.println("回复");
-            session.write("hello");
+            String key = message.toString();
+            if(!hasHandshake && key.indexOf("Key") > 0) {
+                //握手
+                //通过字符串截取获取key值
+                key = key.substring(0, key.indexOf("==") + 2);
+                key = key.substring(key.indexOf("Key") + 4, key.length()).trim();
+                //拼接WEBSOCKET传输协议的安全校验字符串
+                key += "258EAFA5-E914-47DA-95CA-C5AB0DC85B11";
+                //通过SHA-1算法进行更新
+                MessageDigest md = MessageDigest.getInstance("SHA-1");
+                md.update(key.getBytes("utf-8"), 0, key.length());
+                byte[] sha1Hash = md.digest();
+                //进行Base64加密
+                sun.misc.BASE64Encoder encoder = new sun.misc.BASE64Encoder();
+                key = encoder.encode(sha1Hash);
+                //服务器端返回输出内容
+                String aa = "HTTP/1.1 101 Switching Protocols\r\n" +
+                        "Upgrade: websocket\r\n" +
+                        "Connection: Upgrade\r\n" +
+                        "Sec-WebSocket-Accept: " + key +"\r\n\r\n";
+                        //"Sec-WebSocket-Protocol: chat\r\n\r\n";
+                session.write(aa);
+                //session.write("hello");
+                System.out.println("回复");
+            }
         }
     }
 
